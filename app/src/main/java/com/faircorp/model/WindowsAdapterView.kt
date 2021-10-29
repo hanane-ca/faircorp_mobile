ab5f0598c59353aa9d832ef9aa6dc4eabe1e1fb9
eleves <eleves@adm-def-pret09.emse2000.local>
2021-10-30 01:08:09 +0200

    room  details

---
+++ app/src/main/java/com/faircorp/model/WindowsAdapterView.kt
@@ -0,0 +1,53 @@
+package com.faircorp.model
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.faircorp.OnWindowSelectedListener
+import com.faircorp.R
+import com.faircorp.WindowActivity
+
+class WindowAdapter(val listener: OnWindowSelectedListener) : RecyclerView.Adapter<WindowAdapter.WindowViewHolder>() { // (1)
+
+    inner class WindowViewHolder(view: View) : RecyclerView.ViewHolder(view) { // (2)
+        val name: TextView = view.findViewById(R.id.txt_window_name3)
+        val room: TextView = view.findViewById(R.id.txt_window_room)
+        val status: TextView = view.findViewById(R.id.txt_status)
+    }
+
+    private val items = mutableListOf<WindowDto>() // (3)
+
+    fun update(windows: List<WindowDto>) {  // (4)
+        items.clear()
+        items.addAll(windows)
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int = items.size // (5)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WindowViewHolder { // (6)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.activity_windows_item, parent, false)
+        return WindowViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: WindowViewHolder, position: Int) {
+        val window = items[position]
+        holder.apply {
+            name.text = window.name
+            status.text = window.status.toString()
+            room.text = window.room.name
+            itemView.setOnClickListener { listener.onWindowSelected(window.id) } // (1)
+        }
+    }
+
+    override fun onViewRecycled(holder: WindowViewHolder) { // (2)
+        super.onViewRecycled(holder)
+        holder.apply {
+            itemView.setOnClickListener(null)
+        }
+
+    }
+}
